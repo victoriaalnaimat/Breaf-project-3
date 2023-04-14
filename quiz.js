9774de89053bf2a5741648442026f56caa9313f3
victoriaalnaimat <victoriaabadi24@gmail.com>
2023-04-14 03:57:17 +0300

    gde

--- quiz.js
+++ quiz.js
@@ -12,8 +12,6 @@ const update = () => {
     const actives = document.querySelectorAll(".active");
     const width = ((actives.length - 1) / (circles.length - 1)) * 100;
     progress.style.width = `${width}%`;
-
-
 };
 
 let currActive = 1;
@@ -26,3 +24,35 @@ next.addEventListener("click", () => {
     }
     update();
 });
+
+var q1 = document.getElementById("q1");
+var q2 = document.getElementById("q2");
+var q3 = document.getElementById("q3");
+var q4 = document.getElementById("q4");
+
+function myFunction() {
+    if (q1.style.display === "none") {
+        q2.style.display = "none";
+        q3.style.display = "none";
+        q4.style.display = "none";
+        q1.style.display = "block";
+    }
+    else if (q2.style.display === "none") {
+        q2.style.display = "block";
+        q1.style.display = "none";
+        q3.style.display = "none";
+        q4.style.display = "none";
+    }
+    else if (q3.style.display = "none") {
+        q3.style.display = "block";
+        q1.style.display = "none";
+        q2.style.display = "none";
+        q4.style.display = "none";
+    }
+    else if (q4.style.display = "none") {
+        q4.style.display = "block";
+        q1.style.display = "none";
+        q2.style.display = "none";
+        q3.style.display = "none";
+    }
+}
